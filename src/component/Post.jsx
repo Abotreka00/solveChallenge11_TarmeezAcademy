a2fb46a33b93fccc7084ba1f9f5e5bd0d5f295f2
Saleh Hossam <saleh.hossam799@gmail.com>
2025-10-03 23:35:52 +0300

    update to span for Post Component

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -9,7 +9,15 @@ const Post = ({ title, para }) => {
       }}
     >
       <h2 style={{ textAlign: "center" }}>{title}</h2>
-      <span></span>
+      <span
+        style={{
+          display: "block",
+          width: "90%",
+          height: "3px",
+          margin: "0 auto 20px auto",
+          backgroundColor: "#017575",
+        }}
+      ></span>
       <p style={{ textAlign: "center", width: "60%", margin: "auto" }}>
         {para}
       </p>
